4084909917dcd2dc03f3ead2f696be53d0a371ed
Emilly <emillyquessada26@gmail.com>
2025-10-27 14:33:39 -0300

    Rota principal ✔

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #pip install fastapi uvicorn jinja2 python-multipart
-from fastapi import FastAPI, request, Formfrom 
+from fastapi import FastAPI, Request, Formfrom 
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -12,3 +12,18 @@ templates = Jinja2Templates(directory="templates")
 #Definir a pasta onde está os arquivos estáticos (CSS, Imagens e JS)
 app.mount("/static", StaticFiles(directory= "static"))
 
+alunos = [
+    {"nome": "Iago", "nota":8.5},
+    {"nome": "Murilo", "nota":6.5},
+    {"nome": "Francisco", "nota":8.0},
+    {"nome": "Joana", "nota":9.5}
+]
+
+#Rota Principal
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse(
+        "index.html",
+        {"request": request, "lista_alunos": alunos}
+    )
