60dc85d80e7d0ea46663b37ba12b4a11a555397a
Emilly <emillyquessada26@gmail.com>
2025-10-27 16:31:16 -0300

    Rota inicial e index feito ✔

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 #pip install fastapi uvicorn jinja2 python-multipart
-from fastapi import FastAPI, Request, Formfrom 
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+# python -m uvicorn main:app --reload
+
+# http://127.0.0.1:8000
 app = FastAPI(title= "Lista de Alunos")
 
 #Definir a pasta onde está os hyml
 templates = Jinja2Templates(directory="templates")
 
 #Definir a pasta onde está os arquivos estáticos (CSS, Imagens e JS)
-app.mount("/static", StaticFiles(directory= "static"))
+app.mount("/static", StaticFiles(directory= "static"),name="static")
 
 alunos = [
     {"nome": "Iago", "nota":8.5},
     {"nome": "Murilo", "nota":6.5},
-    {"nome": "Francisco", "nota":8.0},
-    {"nome": "Joana", "nota":9.5}
+    {"nome": "Joana", "nota":9.5},
+    {"nome": "Francisco", "nota":8.0}
 ]
 
 #Rota Principal
